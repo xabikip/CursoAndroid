516d8efe31ffd555d15c3ae205ab68074266f482
Xabi Pico <xabikip@gmail.com>
2015-02-04 09:31:56 +0100

    Eliminar imports no usados

--- ComponentListView/src/com/example/componentlistview/MainActivity.java
+++ ComponentListView/src/com/example/componentlistview/MainActivity.java
@@ -1,18 +1,15 @@
 package com.example.componentlistview;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import android.app.AlertDialog;
 import android.app.ListActivity;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
 
 public class MainActivity extends ListActivity{
